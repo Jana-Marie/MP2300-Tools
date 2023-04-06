07aa753d1b7c16274c824e20bba96fca841a2a48
Jana Marie <janamarie@janamarie.dev>
2023-04-06 21:58:49 +0200

    proper implementation of relative and absolute

--- hpgl2gpgl.py
+++ hpgl2gpgl.py
@@ -22,40 +22,39 @@ print("Reading file: {}".format(sys.argv[1]))
 print("Now processing {} lines of HPGL to GPGL".format(len(commands)-1))
 
 mode = 0 # 0 = absolute, 1 = relative
-out = ""
+out = "" # output string, written to file
 
 for index,c in enumerate(commands[:len(commands)-1]):
-	if c[:2] == "IN":
+	if c[:2] == "IN": # init
 		out += '\x1B'
-		out += '\x03'		
-		out += "FU3564,5268"	
-		out += '\x03'	
-		out += "FM1"	
-		out += '\x03'			
-		out += "TB50,1"	
-		out += '\x03'			
-		out += "FO3564"	
+		out += '\x03'
+		out += "FU3564,5268"
+		out += '\x03'
+		out += "FM1"
+		out += '\x03'
+		out += "TB50,1"
+		out += '\x03'
+		out += "FO3564"
 		out += '\x03'
 		out += "&100,100,100,\0,0,Z5588,4064,L0,!{}.0".format(speed)	
-		out += '\x03'	
-		out += "FX8,0"	
 		out += '\x03'
-	elif c[:2] == "PD":
-		if mode == 0:
+		out += "FX8,0"
+		out += '\x03'
+	elif c[:2] == "PD": # pen down → draw
+		if mode == 0: # draw in relative 
 			out += "D"
 		else:
 			out += "E"
 		for sc in c[2:]:
 			out += sc
 		out += '\x03'
-	elif c[:2] == "PA":
-		if mode == 0:
+	elif c[:2] == "PA": # plot absolute
+		mode = 0 # switch to absolute mode
+		if len(c) > 2:
 			out += "M"
-		else:
-			out += "O"
-		for sc in c[2:]:
-			out += sc
-		out += '\x03'
+			for sc in c[2:]:
+				out += sc
+			out += '\x03'
 	elif c[:2] == "PU":
 		if mode == 0:
 			out += "M"
@@ -71,10 +70,13 @@ for index,c in enumerate(commands[:len(commands)-1]):
 		out += "!"
 		out += c[2:]
 		out += '\x03'
-	elif c[:2] == "PR":
+	elif c[:2] == "PR": # switch to relative movement
 		mode = 1
-	elif c[:2] == "PA":
-		mode = 0
+		if len(c) > 2:
+			out += "O"
+			for sc in c[2:]:
+				out += sc
+			out += '\x03'
 	else:
 		commandsMissing.append(c)
 		pass
